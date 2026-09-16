645de5c0838bd65d784784e4de93c54ab1789fc9
damagueta <damagueta@gmail.com>
2026-09-16 14:06:01 -0300

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = dict()
+
+        for palavra in strs:
+            sinalizado = "".join(sorted(palavra))
+
+            if sinalizado not in seen:
+                seen[sinalizado] = []
+            seen[sinalizado].append(palavra)
+                 
+        return list(seen.values())
